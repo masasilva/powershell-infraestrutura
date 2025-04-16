9972b76db91c5b11625280c0042171c6c141fc9f
masasilva <127128345+masasilva@users.noreply.github.com>
2025-04-15 21:37:47 -0300

    Commit

---
+++ inventario/exportar-infos-hardware.ps1
@@ -0,0 +1,15 @@
+﻿# Exportar informações de hardware dos computadores
+# Matheus Antonio da Silva
+
+# Obter informações de hardware usando WMI
+$computers = Get-WmiObject -Class Win32_ComputerSystem
+
+# Obter informações sobre processadores
+$cpu = Get-WmiObject -Class Win32_Processor
+
+# Exportar para um arquivo CSV
+$computers | Select-Object Name, Manufacturer, Model, TotalPhysicalMemory |
+    Export-Csv -Path "C:\Relatorios\Hardware_Inventory.csv" -NoTypeInformation
+
+$cpu | Select-Object Name, NumberOfCores, NumberOfLogicalProcessors |
+    Export-Csv -Path "C:\Relatorios\CPU_Inventory.csv" -NoTypeInformation
