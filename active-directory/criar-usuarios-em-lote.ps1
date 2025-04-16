07ccef5384dd1cd781fa32014ab24ad08925c031
masasilva <127128345+masasilva@users.noreply.github.com>
2025-04-15 21:36:04 -0300

    Commit

---
+++ active-directory/criar-usuarios-em-lote.ps1
@@ -0,0 +1,15 @@
+﻿# Criação em massa de usuários no AD
+# Entrada: CSV com Nome, Sobrenome, Login
+
+Import-Csv .\usuarios.csv | ForEach-Object {
+    $SamAccountName = $_.Login
+    $NomeCompleto = "$($_.Nome) $($_.Sobrenome)"
+    $Senha = ConvertTo-SecureString "Senha123!" -AsPlainText -Force
+
+    New-ADUser -Name $NomeCompleto `
+               -SamAccountName $SamAccountName `
+               -UserPrincipalName "$SamAccountName@empresa.com" `
+               -AccountPassword $Senha `
+               -Enabled $true `
+               -Path "OU=Colaboradores,DC=empresa,DC=com"
+}
---
+++ sccm/atualizar-winrar.ps1
@@ -0,0 +1,18 @@
+﻿# Atualiza o WinRAR via SCCM
+# Matheus Antonio da Silva
+
+$AppName = "WinRAR"
+$NewVersion = "6.24"
+$SourcePath = "\\servidor\SCCM\Pacotes\WinRAR\$NewVersion"
+$DeployCollection = "Workstations - Produção"
+
+# 1. Importar módulo do SCCM
+Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+Set-Location "SCCM:"
+
+# 2. Criar nova aplicação
+New-CMApplication -Name "$AppName $NewVersion" -AutoInstall $true
+
+# 3. Distribuir e implantar
+Start-CMContentDistribution -ApplicationName "$AppName $NewVersion" -DistributionPointGroupName "Todos DPs"
+Start-CMApplicationDeployment -CollectionName $DeployCollection -Name "$AppName $NewVersion" -DeployAction Install -DeployPurpose Available
